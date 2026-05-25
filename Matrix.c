80e5f98219a5fb5d5363c0e28eb2c2e4805fe73b
Eng-mohamed-debug <mohamedabdo.business2025@gmail.com>
2026-05-26 02:15:21 +0300

    implement create matrix and areequal and multiplyScalar

---
+++ Matrix.c
@@ -0,0 +1,46 @@
+#include "Matrix.h"
+
+Matrix createMatrix(int rows, int cols)
+{
+    Matrix mat;
+    mat.rows = rows;
+    mat.cols = cols;
+    mat.is_square = (rows == cols);
+
+    mat.data = malloc(rows * sizeof(double*));  // Each row will be a pointer to an array (double pointer)
+    for(int i = 0; i < rows; i++)
+    {
+        mat.data[i] = calloc(cols, sizeof(double));  // all elements will be initialized to zero
+    }
+    return mat;
+};
+
+int areEqual(Matrix A, Matrix B)
+{
+    if(A.rows != B.rows || A.cols != B.cols)
+    {
+        return 0;
+    }
+    for(int i = 0; i < A.rows; i++)
+    {
+        for(int j = 0; j < A.cols; j++)
+        {
+            if(A.data[i][j] != B.data[i][j])
+            {
+                return 0;
+            } 
+        }
+    }
+    return 1;
+}
+
+void multiplyScalar(Matrix mat, double scalar)
+{
+    for(int i = 0; i < mat.rows; i++)
+    {
+        for(int j = 0; j < mat.cols; j++)
+        {
+            mat.data[i][j] *= scalar;
+        }
+    }
+}
---
+++ Matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct {
+    unsigned int rows;
+    unsigned int cols;
+    int is_square;
+    double** data;
+} Matrix;
+
+Matrix createMatrix(int rows, int cols);      // Create a matrix  
+void printMat(Matrix mat);                   // function to print the matrix
+Matrix addMatrices(Matrix A, Matrix B);      // to add two matrices together
+Matrix transpose(Matrix mat);                // transpose a matrix 
+int areEqual(Matrix A,  Matrix B);           // Checks if two matrices are equal
+void multiplyScalar(Matrix mat, double scalar); // scalar multiplication
+#endif
